d89c95c28f95abc7d0879e282ef43bbf6457ca2d
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-09 13:35:47 -0400

    workout.js started. exercises array added with type name duration weight reps and distance

---
+++ models/workout.js
@@ -0,0 +1,30 @@
+const mongoose = mongoose.mongoose
+
+
+exercises: [
+    {
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter a type for the exercise"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter a name for the exercise"
+        },
+        duration: {
+            type: Number,
+            required: "please enter duration"
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        distance: {
+            type: Number,
+        },
+    }
+]
