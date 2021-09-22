b051f2c5219540bafbb08c9282cae46dc8b22f66
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-22 01:09:34 -0400

    added module.exports and schema on line 2

--- models/workout.js
+++ models/workout.js
@@ -1,30 +1,32 @@
-const mongoose = mongoose.mongoose
-
+const mongoose = mongoose.mongoose;
+const { Schema } = mongoose;
 
 exercises: [
-    {
-        type: {
-            type: String,
-            trim: true,
-            required: "Enter a type for the exercise"
-        },
-        name: {
-            type: String,
-            trim: true,
-            required: "Enter a name for the exercise"
-        },
-        duration: {
-            type: Number,
-            required: "please enter duration"
-        },
-        weight: {
-            type: Number,
-        },
-        reps: {
-            type: Number,
-        },
-        distance: {
-            type: Number,
-        },
-    }
-]
+  {
+    type: {
+      type: String,
+      trim: true,
+      required: "Enter a type for the exercise",
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: "Enter a name for the exercise",
+    },
+    duration: {
+      type: Number,
+      required: "please enter duration",
+    },
+    weight: {
+      type: Number,
+    },
+    reps: {
+      type: Number,
+    },
+    distance: {
+      type: Number,
+    },
+  },
+];
+
+module.exports = Workout;
