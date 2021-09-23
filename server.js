b24d8ad05bb438083042fd6489ab8fba59dfb547
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-23 00:12:44 -0400

    updated server.js with session, more routes, static setup and an event listener

--- server.js
+++ server.js
@@ -4,7 +4,31 @@ const app = express();
 const mongoose = require("mongoose");
 const routes = require("./controllers");
 const path = require("path");
+const session = require("express-session");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessDB", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+});
 
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: true,
+	cookie: {},
+};
+app.use(session(sess));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(routes);
+
+app.listen(PORT, () => {
+	console.log(`Now listening on ${PORT}`);
+});
