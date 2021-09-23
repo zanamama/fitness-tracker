da33938595fc427f87c05926911b8890e1087591
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-22 20:34:53 -0400

    added require dotenv and port connection to server.js file in root folder

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const routes = require("./controllers");
+const path = require("path");
 
-
-
-
-
+const PORT = process.env.PORT || 3001;
 
 app.use(routes);
