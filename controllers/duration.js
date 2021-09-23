9ccfbef426e13fb9db4b05f53912e6bc67ee2ae7
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-23 00:14:32 -0400

    added duration.js file under controllers

---
+++ controllers/duration.js
@@ -0,0 +1,13 @@
+const calculateDuration = (workouts) => {
+	const modifiedWorkouts = workouts.map((workout) => {
+		let sumDuration = 0;
+		workout.exercises.map((exercise) => {
+			sumDuration += exercise.duration;
+		});
+		let modifiedExercise = { ...workout._doc, totalDuration: sumDuration };
+		return modifiedExercise;
+	});
+	return modifiedWorkouts;
+};
+
+module.exports = { calculateDuration };
